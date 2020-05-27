02486eb557ac46bb619147a677e3f0bbda47c0db
Drahgos <alex.camargo.azevedo@gmail.com>
2020-05-26 22:00:30 -0300

    Revert "Firt Commit"
    This reverts commit 5e870d15585d0277bc9c1f2c3bc181275aaa1b71.

--- front/src/app/model/usuario.model.ts
+++
@@ -1,7 +0,0 @@
-export class Usuario {
-  public Id: String;
-  public nome: String;
-  public email: String;
-  public login: String;
-  public senha: String;
-}
--- src/ApiCoreEF.Domain/User/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ApiCoreEF.Domain.Interfaces;
-
-namespace ApiCoreEF.Domain.User
-{
-    public interface IUserRepository : IRepository<User>
-    {
-
-    }
-}
